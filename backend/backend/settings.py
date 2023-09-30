1a062b4d319a7f32853b11f1eab07e62097a2fe3
Сергей Кара <greykara@mail.ru>
2023-10-01 01:04:18 +0300

    rev1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='default-value')
 
 DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['localhost', 'backend', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
